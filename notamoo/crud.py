c658496fd893c2c96fdee19fe25dc4f2cac7f4dc
Artin Mohammadi <sheikhartin@gmail.com>
2024-08-04 20:49:11 +0330

    To read a note and update its status

--- notamoo/crud.py
+++ notamoo/crud.py
@@ -1,20 +1,64 @@
 import uuid
+from typing import Optional
 
 from sqlalchemy.orm import Session
 
 from notamoo import schemas, models
-from notamoo.utils import get_password_hash
+from notamoo.utils import get_password_hash, verify_password
+from notamoo.exceptions import (
+    NoteNotFoundException,
+    NoteExpiredException,
+    NoteViewLimitReachedException,
+    PasswordMismatchException,
+)
+
+
+def _create_note_slug(db: Session) -> str:
+    while True:
+        slug = uuid.uuid4().hex[:15]
+        if (
+            db.query(models.Note).filter(models.Note.slug == slug).first()
+        ) is None:
+            return slug
 
 
 def create_note(db: Session, note: schemas.NoteCreate) -> models.Note:
     db_note = models.Note(
         content=note.content,
-        password=get_password_hash(note.password) if note.password else None,
+        password=(
+            get_password_hash(note.password)
+            if note.password is not None
+            else None
+        ),
         view_limit=note.view_limit,
-        slug=uuid.uuid4().hex[:15],
+        slug=_create_note_slug(db),
         expires_at=note.expires_at,
     )
     db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
+
+
+def read_and_update_note_views(
+    db: Session,
+    slug: str,
+    password: Optional[str] = None,
+) -> models.Note:
+    db_note = db.query(models.Note).filter(models.Note.slug == slug).first()
+    if db_note is None:
+        raise NoteNotFoundException(slug)
+    if db_note.is_expired():
+        raise NoteExpiredException()
+    if db_note.has_reached_view_limit():
+        raise NoteViewLimitReachedException()
+    if (
+        db_note.password is not None
+        and password is not None
+        and not verify_password(password, db_note.password)
+    ):
+        raise PasswordMismatchException()
+    db_note.views += 1
     db.commit()
     db.refresh(db_note)
     return db_note
