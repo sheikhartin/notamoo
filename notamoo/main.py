f3d16c64ea36f75d75009c661c4ee3256e579772
Artin Mohammadi <sheikhartin@gmail.com>
2024-08-13 12:16:42 +0330

    Using CORS middleware

--- notamoo/main.py
+++ notamoo/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from notamoo.database import Base, engine
 from notamoo.routers import notes
@@ -20,6 +21,13 @@ app = FastAPI(
     docs_url='/api/docs/swagger',
     redoc_url='/api/docs/redoc',
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(notes.router, prefix='/api/notes', tags=['Notes'])
